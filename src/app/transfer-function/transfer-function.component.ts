a48c14777188091a0976674523bfe664f825f6ae
Saud <saudd2gh@gmail.com>
2018-12-20 08:58:37 +0800

    mathjax initial test success

--- src/app/transfer-function/transfer-function.component.ts
+++ src/app/transfer-function/transfer-function.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BlockComponent } from '../util/util';
 import * as math from '../../../node_modules/mathjs/dist/math.min.js';
-//import * as mathjax from '../../../node_modules/mathjax/MathJax.js';
 
 
 @Component({
@@ -12,23 +11,21 @@ import * as math from '../../../node_modules/mathjs/dist/math.min.js';
 
 export class TransferFunctionComponent implements BlockComponent, OnInit {
 
-  input = 'x^2';
+  input = 't^2';
   output = '';
-  private transFunction:string = '(5x+6)';
-  dispResult;
-  simpResult;
-  tex;
-  jax;
+  private transFunction:string = '(5t+6)';
+  private tex;
+  private parsedOutput;
 
   constructor() {
     this.output = this.input + '*' + this.transFunction;
-    this.dispResult = math.parse(this.output);
-    this.simpResult = math.simplify(this.dispResult);
-    this.tex = this.dispResult.toTex(this.simpResult);
-    //this.jax = mathjax.tex2jax(this.tex);
+    this.parsedOutput = math.parse(this.output);
+    this.tex = this.parsedOutput.toTex();
   }
 
-  determineOutput(){}
+  determineOutput(){
+    
+  }
 
   ngOnInit() {
   }
